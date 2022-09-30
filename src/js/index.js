5770a27f33d1384160eb52355ffc18f8b6189da8
zlavvitch <z.kosarevitch@yandex.ru>
2022-09-30 19:46:17 +0300

    version 1.0 (lab2)

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 'use strict';
 
 import '../scss/style.scss';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+// import bootstrap from 'bootstrap';
+// import '../../node_modules/bootstrap/js/dist/util.js';
+// import '../../node_modules/bootstrap/js/dist/modal.js';
+
+// const carousel = new bootstrap.Carousel('#myCarousel');
+
+let myCarousel = document.querySelector('#myCarousel');
+let carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false,
+  visibleItems: 4,
+  itemsToScroll: 1,
+});
